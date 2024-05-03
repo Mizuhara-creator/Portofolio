5cf476ac083c47bd4fc6003617d7509af8834ac7
Arda <romansaardnaap@gmail.com>
2024-05-04 00:24:56 +0700

    init commit

---
+++ javas.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Mencegah pengiriman form
+    alert('Terima kasih telah menghubungi kami!');
+});
